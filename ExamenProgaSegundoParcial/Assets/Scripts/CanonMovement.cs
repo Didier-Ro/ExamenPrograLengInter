1a4d4589fecc67c2608a77c1aa0a1b28c35c2bff
Didier-Ro <ortizrojeroh@gmail.com>
2022-04-06 19:21:41 -0500

    first commit

---
+++ ExamenProgaSegundoParcial/Assets/Scripts/CanonMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CanonMovement : MonoBehaviour
+{
+    [SerializeField] private Camera _camera = default;
+
+    void Update()
+    {
+        Vector3 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0.0f;
+
+        LookAt2D(mousePosition);
+    }
+
+    private void LookAt2D(Vector3 pos)
+    {
+        Vector3 dir = pos - transform.position;
+        dir.Normalize();
+        transform.rotation = Quaternion.FromToRotation(Vector3.right, dir);
+    }
+}
